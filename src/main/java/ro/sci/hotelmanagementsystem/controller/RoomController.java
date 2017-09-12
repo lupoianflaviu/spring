523d7fa39ed1eb484584d8dbbbc9be28b12c2b67
Flaviu Lupoian <flaviu.lupoian@jpard.com>
2017-09-12 13:32:18 +0300

    HotelManagement Structure v1.0

---
+++ src/main/java/ro/sci/hotelmanagementsystem/controller/RoomController.java
@@ -0,0 +1,7 @@
+package ro.sci.hotelmanagementsystem.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RoomController {
+}
---
+++ src/test/java/ro/sci/hotelmanagementsystem/TempTest.java
@@ -0,0 +1,4 @@
+package ro.sci.hotelmanagementsystem;
+
+public class TempTest {
+}
