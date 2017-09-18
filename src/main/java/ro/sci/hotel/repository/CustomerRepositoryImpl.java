115a47dbcff805dc22b0d1b7953e737f0625ad9c
andreivintila <vintila.andrei.av@gmail.com>
2017-09-18 19:38:25 +0300

    update Customer Repo and Service with implementations

--- src/main/java/ro/sci/hotel/repository/CustomerRepositoryImpl.java
+++ src/main/java/ro/sci/hotel/repository/CustomerRepositoryImpl.java
@@ -2,11 +2,46 @@ package ro.sci.hotel.repository;
 
 
 import org.springframework.stereotype.Repository;
+import ro.sci.hotel.model.customer.Customer;
+
+import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Customer repository implementation
  */
 @Repository
-public class CustomerRepositoryImpl extends BaseRepository implements CustomerRepository{
+public class CustomerRepositoryImpl extends BaseRepository implements CustomerRepository<Customer>{
 
+    private static final Logger LOGGER = Logger.getLogger("Hotel");
+
+    @Override
+    public List<Customer> getAll() {
+        return null;
+    }
+
+    @Override
+    public void create(Customer customer) {
+
+    }
+
+    @Override
+    public void delete(Customer customer) {
+
+    }
+
+    @Override
+    public void update(Customer customer) {
+
+    }
+
+    @Override
+    public List<Customer> searchByCustomerId(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> searchByFirstName(String firstName) {
+        return null;
+    }
 }
--- src/main/java/ro/sci/hotel/service/CustomerServiceImpl.java
+++ src/main/java/ro/sci/hotel/service/CustomerServiceImpl.java
@@ -1,11 +1,46 @@
 package ro.sci.hotel.service;
 
 import org.springframework.stereotype.Service;
+import ro.sci.hotel.model.customer.Customer;
+import ro.sci.hotel.repository.CustomerRepository;
+
+import java.util.List;
 
 /**
  * Customer service implementation
  */
 @Service
-public class CustomerServiceImpl implements CustomerService {
+public class CustomerServiceImpl implements CustomerService<Customer> {
 
+    private CustomerRepository<Customer> customerRepository;
+
+    @Override
+    public List<Customer> getAll() {
+        return this.customerRepository.getAll();
+    }
+
+    @Override
+    public void create(Customer customer) {
+        this.customerRepository.create(customer);
+    }
+
+    @Override
+    public void delete(Customer customer) {
+        this.customerRepository.delete(customer);
+    }
+
+    @Override
+    public void update(Customer customer) {
+        this.customerRepository.update(customer);
+    }
+
+    @Override
+    public List<Customer> searchByCustomerId(int id) {
+        return this.customerRepository.searchByCustomerId(id);
+    }
+
+    @Override
+    public List<Customer> searchByFirstName(String firstName) {
+        return this.customerRepository.searchByFirstName(firstName);
+    }
 }
